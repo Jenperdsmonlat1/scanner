6653c182a894a594a9f9b5a82ea0c1279e5c78f0
Cat-Man123 <65119790+Cat-Man123@users.noreply.github.com>
2022-05-30 15:32:42 +0200

    Update main.c

--- windows/main.c
+++ windows/main.c
@@ -1,1 +1,40 @@
+#include <winsock2.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+#pragma comment(lib, "ws2_32.lib")
+
+int main(int argc, char *argv[]) {
+  
+  char ip[16];
+  int connection;
+  
+  if (argc > 1) {
+    printf("IP to scan: %s", ip);
+    strcpy(ip, argv[1]);
+  }
+  
+  WSADATA WSAData;
+  WSAStartup(MAKEWORD(2, 0), &WSAData);
+  
+  SOCKET sock;
+  SOCKADDR_IN sin;
+  
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+  sin.sin_addr.s_addr = inet_addr(ip);
+  sin.sin_family = AF_INET;
+  
+  for (int i = 0; i <= 65535; i++) {
+    sin.sin_port = htons(i);
+    connection = connect(sock, (SOCKADDR *)&sin, sizeof(sin));
+    
+    if (connection < 0) {
+      printf("Le port %d est fermé.", i);
+    } else {
+      printf("Le port %d est ouvert.", i);
+    }
+  }
+  
+  WSACleanup();
+  return 0;
+}
